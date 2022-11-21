8b20b85c87c1bda3cbea809e202276a3f8bbb072
thordurf <thordur921@gmail.com>
2022-11-21 11:17:26 +0100

    scss files for each section

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import "./index.css";
+import "./styles/header.scss";
+import "./styles/info.scss";
+import "./styles/inout.scss";
+import "./styles/news.scss";
+import "./styles/footer.scss";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
-)
+);
