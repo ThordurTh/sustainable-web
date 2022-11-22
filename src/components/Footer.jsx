71806d64669a1f99cc93d47998915280a615d053
Mehmet <mehmet.h.yurdakul@gmail.com>
2022-11-22 09:57:27 +0100

    footer done

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,7 +1,30 @@
 import React from "react";
 import "../styles/footer.scss";
 function Footer() {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-wrapper">
+      <div className="address">
+        <h3>ADDRESS /</h3>
+        <p>Gammel Køngevej 3E</p>
+        <p>1610 Kobenhavn V</p>
+        <p>Danmark</p>
+        <p>VAT: DK20212209</p>
+      </div>
+      <div className="contact">
+        <h3>CONTACT /</h3>
+        <p>+45 33 42 21 00</p>
+        <p>connect@adviceagency.com</p>
+      </div>
+
+      <div className="social">
+        <h3>SOCIAL /</h3>
+        <p>Facebook</p>
+        <p>Twitter</p>
+        <p>LinkedIn</p>
+        <p>Instagram</p>
+      </div>
+    </div>
+  );
 }
 
 export default Footer;
