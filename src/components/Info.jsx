3ada769db7a63bf4e0706e93241f5626505edd03
Mehmet <mehmet.h.yurdakul@gmail.com>
2022-11-24 11:23:41 +0100

    info section - done

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -6,14 +6,17 @@ function Info() {
   return (
     <section className="info-section">
       <article>
-        <h1>Sustainability?</h1>
+        <h1>
+          WE WANT TO SHAPE SUSTAINABLE CONSUMPTION, ATTITUDES AND BEHAVIOUR.
+        </h1>
         <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum
-          reprehenderit, pariatur, deserunt quaerat magnam harum ex illo labore
-          et iure neque minima laboriosam earum distinctio praesentium facilis
-          dignissimos suscipit sunt! Lorem ipsum dolor sit amet consectetur
-          adipisicing elit. Nostrum reprehenderit, pariatur, deserunt quaerat
-          magnam harum ex illo labore et iure neque minima.
+          We want to shape sustainable consumption, attitudes and behaviour. In
+          2025, we will only work for companies that contribute to a sustainable
+          future and with projects that contribute to a sustainable present. We
+          are all in this together.
+          <br />
+          By the way, you can use our CO2e calculator below to see how much CO2e
+          your daily internet usage produce.
         </p>
       </article>
       <img src={servers}></img>
